9f4da1d445aaad451195e448b7540ae023b760e8
huanfeng <wwwdufeng@gmail.com>
2023-11-24 17:58:32 +0800

    feat: 完成基本功能, 增加命令行处理

--- lib/apk_info.dart
+++ lib/apk_info.dart
@@ -2,25 +2,242 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:developer';
 
-Future<void> getApkInfo() async {
-  log("getApkInfo: start");
-  // 启动子进程
-  Process process = await Process.start('python', ['--version']);
+import 'config.dart';
 
-// 监听stdout
-  process.stdout.listen((List<int> data) {
-    String output = utf8.decode(data);
-    log(output);
-    // 从output提取信息
-  });
+Future<ApkInfo?> getApkInfo(String apk) async {
+  log("getApkInfo: apk=$apk start");
+  final aapt = Config.aapt2Path;
 
-// 监听stderr
-  process.stderr.listen((List<int> data) {
-    String output = utf8.decode(data);
-    log(output);
-  });
+  final start = DateTime.now();
+  var result = await Process.run(aapt, ['dump', 'badging', apk],
+      stdoutEncoding: utf8, stderrEncoding: utf8);
+  final end = DateTime.now();
+  var exitCode = result.exitCode;
+  final cost = end.difference(start).inMilliseconds;
+  log("getApkInfo: end exitCode=$exitCode, cost=${cost}ms");
+  if (exitCode == 0) {
+    final apkInfo = parseApkInfoFromOutput(result.stdout.toString());
+    return apkInfo;
+  }
+  return null;
+}
 
-// 等待子进程退出
-  var exitCode = await process.exitCode;
-  log("getApkInfo: end exitCode=$exitCode");
+ApkInfo parseApkInfoFromOutput(String output) {
+  final lines = output.split("\n");
+  final apkInfo = ApkInfo();
+  for (final (index, item) in lines.indexed) {
+    log("[$index] $item");
+    apkInfo.parseLine(item);
+  }
+  log("apkInfo=$apkInfo");
+  return apkInfo;
+}
+
+final _kNoneSingleQuotePattern = RegExp(r"[^']");
+
+extension StringExt on String {
+  // 去除前后的单引号
+  String trimSQ() {
+    final start = indexOf(_kNoneSingleQuotePattern);
+    final end = lastIndexOf(_kNoneSingleQuotePattern);
+    return substring(start < 0 ? 0 : start, end < 0 ? length : end + 1);
+  }
+}
+
+class ApkInfo {
+  String apkPath = "";
+  int apkSize = 0;
+
+  String? packageName;
+  int? versionCode;
+  String? versionName;
+  String? platformBuildVersionName;
+  int? platformBuildVersionCode;
+  int? compileSdkVersion;
+  String? compileSdkVersionCodename;
+  int? sdkVersion;
+  int? targetSdkVersion;
+  String? label;
+  Map<String, String> labels = {};
+  List<String> usesPermissions = [];
+  Map<String, String> icons = {};
+  Component application = Component();
+  List<Component> launchableActivity = [];
+  List<String> userFeatures = [];
+  List<String> userFeaturesNotRequired = [];
+  List<String> userImpliedFeatures = [];
+  List<String> supportsScreens = [];
+  List<String> locales = [];
+  List<String> densities = [];
+  bool? supportsAnyDensity;
+  List<String> nativeCodes = [];
+
+  List<String> others = [];
+
+  (String, String) parseToKeyValue(String line, String separator) {
+    final pos = line.indexOf(separator);
+    if (pos != -1) {
+      final key = line.substring(0, pos).trim();
+      final value = line.substring(pos + 1).trim();
+      return (key, value);
+    }
+    return (line.trim(), "");
+  }
+
+  (String, String) parseLineToKeyValue(String line) {
+    return parseToKeyValue(line, ":");
+  }
+
+  (String, String) parseValueToKeyValue(String line) {
+    return parseToKeyValue(line, "=");
+  }
+
+  String? parseString(String line) {
+    final items = line.split(":");
+    if (items.length == 2) {
+      return items[1].trim();
+    } else {
+      others.add(line);
+    }
+    return null;
+  }
+
+  int? parseInt(String value) {
+    return int.tryParse(value.trimSQ());
+  }
+
+  String parseValueForName(String text) {
+    final (_, value) = parseValueToKeyValue(text);
+    return value.trimSQ();
+  }
+
+  void parseLine(String line) {
+    final (key, value) = parseLineToKeyValue(line);
+    switch (key) {
+      case "package":
+        parsePackage(value);
+        break;
+      case "sdkVersion":
+        sdkVersion = parseInt(value);
+        break;
+      case "targetSdkVersion":
+        targetSdkVersion = parseInt(value);
+        break;
+      case "application-label":
+        label = value.trimSQ();
+        break;
+      case "uses-permission":
+        usesPermissions.add(parseValueForName(value));
+        break;
+      case "application":
+        parseComponent(value, application);
+        break;
+      case "launchable-activity":
+        final component = Component();
+        parseComponent(value, component);
+        launchableActivity.add(component);
+        break;
+      case "supports-screens":
+        parseStringList(value, supportsScreens);
+        break;
+      case "locales":
+        parseStringList(value, locales);
+        break;
+      case "densities":
+        parseStringList(value, densities);
+        break;
+      case "supports-any-density":
+        supportsAnyDensity = value.trimSQ() == "true";
+        break;
+      case "native-code":
+        parseStringList(value, nativeCodes);
+        break;
+      default:
+        {
+          if (key.startsWith("application-label-")) {
+            labels[key.substring("application-label-".length + 1)] =
+                value.trimSQ();
+          } else if (key.startsWith("application-icon-")) {
+            icons[key.substring("application-icon-".length + 1)] =
+                value.trimSQ();
+          } else {
+            others.add(line);
+          }
+          break;
+        }
+    }
+  }
+
+  void parsePackage(String text) {
+    final items = text.split(" ");
+    for (final item in items) {
+      final (key, value) = parseValueToKeyValue(item);
+      switch (key) {
+        case "name":
+          packageName = value.trimSQ();
+          break;
+        case "versionCode":
+          versionCode = parseInt(value);
+          break;
+        case "versionName":
+          versionName = value.trimSQ();
+          break;
+        case "platformBuildVersionName":
+          platformBuildVersionName = value.trimSQ();
+          break;
+        case "platformBuildVersionCode":
+          platformBuildVersionCode = parseInt(value);
+          break;
+        case "compileSdkVersion":
+          compileSdkVersion = parseInt(value);
+          break;
+        case "compileSdkVersionCodename":
+          compileSdkVersionCodename = value.trimSQ();
+          break;
+      }
+    }
+  }
+
+  void parseComponent(String value, Component component) {
+    final items = value.split(" ");
+    for (final item in items) {
+      final (key, value) = parseValueToKeyValue(item);
+      switch (key) {
+        case "name":
+          component.name = value.trimSQ();
+          break;
+        case "label":
+          component.label = value.trimSQ();
+          break;
+        case "icon":
+          component.icon = value.trimSQ();
+          break;
+      }
+    }
+  }
+
+  void parseStringList(String value, List<String> out) {
+    final items = value.split(" ");
+    for (final item in items) {
+      out.add(item.trimSQ());
+    }
+  }
+
+  @override
+  String toString() {
+    return 'ApkInfo{apkPath: $apkPath, apkSize: $apkSize, packageName: $packageName, versionCode: $versionCode, versionName: $versionName, platformBuildVersionName: $platformBuildVersionName, platformBuildVersionCode: $platformBuildVersionCode, compileSdkVersion: $compileSdkVersion, compileSdkVersionCodename: $compileSdkVersionCodename, sdkVersion: $sdkVersion, targetSdkVersion: $targetSdkVersion, label: $label, labels: $labels, usesPermissions: $usesPermissions, icons: $icons, application: $application, launchableActivity: $launchableActivity, userFeatures: $userFeatures, userFeaturesNotRequired: $userFeaturesNotRequired, userImpliedFeatures: $userImpliedFeatures, supportsScreens: $supportsScreens, locales: $locales, densities: $densities, supportsAnyDensity: $supportsAnyDensity, nativeCodes: $nativeCodes, others: $others}';
+  }
+}
+
+class Component {
+  String? name;
+  String? label;
+  String? icon;
+
+  Component({this.name, this.label, this.icon});
+
+  @override
+  String toString() {
+    return 'Component{name: $name, label: $label, icon: $icon}';
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:apk_info_tool/apk_info.dart';
 import 'package:apk_info_tool/setting.dart';
 import 'package:flutter/foundation.dart';
@@ -12,6 +14,8 @@ import 'config.dart';
 
 const appTitle = "APK Info Tool";
 
+String apkByArgs = "";
+
 bool get isDesktop {
   if (kIsWeb) return false;
   return [
@@ -21,16 +25,22 @@ bool get isDesktop {
   ].contains(defaultTargetPlatform);
 }
 
-void main() async {
+void main(List<String> arguments) async {
   await Config.init();
   Config.loadConfig();
+
+  log("args=$arguments");
+  if (arguments.isNotEmpty) {
+    apkByArgs = arguments.first;
+  }
 
   WidgetsFlutterBinding.ensureInitialized();
 
   if (isDesktop) {
     await windowManager.ensureInitialized();
     windowManager.waitUntilReadyToShow().then((_) async {
-      await windowManager.setMinimumSize(const Size(500, 600));
+      await windowManager.setMinimumSize(const Size(400, 400));
+      await windowManager.setSize(const Size(500, 600));
       await windowManager.setTitle(appTitle);
       await windowManager.show();
       // await windowManager.setPreventClose(true);
@@ -50,8 +60,7 @@ class _APKInfoPageState extends State<APKInfoPage> {
   FilePickerResult? filePickerResult;
   String? selectedFilePath;
   int? fileSize;
-
-  TextEditingController packageNameController = TextEditingController(text: '');
+  ApkInfo? apkInfo;
 
   void openFilePicker() async {
     var result = await FilePicker.platform.pickFiles(
@@ -63,11 +72,51 @@ class _APKInfoPageState extends State<APKInfoPage> {
     // 打开文件选择
     if (file != null) {
       log('filePaths=$file');
-      setState(() {
-        fileSize = file.size;
-        selectedFilePath = file.path;
-        WindowManager.instance.setTitle(file.name);
+      if (file.path != null) {
+        openApk(file.path!, file.size);
+      }
+    }
+  }
+
+  void openApk(String path, int size) {
+    setState(() {
+      fileSize = size;
+      selectedFilePath = path;
+      if (selectedFilePath != null) {
+        loadApkInfo();
+      }
+    });
+  }
+
+  void loadApkInfo() {
+    if (selectedFilePath != null) {
+      if (Config.aapt2Path.isEmpty) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(const SnackBar(content: Text("请先设置 aapt2 路径")));
+        return;
+      }
+      getApkInfo(selectedFilePath!).then((value) {
+        if (value == null) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(const SnackBar(content: Text("解析APK信息失败!")));
+        } else {
+          setState(() {
+            apkInfo = value;
+          });
+        }
       });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    log("initState apkByArgs=$apkByArgs", level: 2);
+    if (apkByArgs.isNotEmpty) {
+      final file = File(apkByArgs);
+      if (file.existsSync()) {
+        openApk(file.path, file.lengthSync());
+      }
     }
   }
 
@@ -86,13 +135,14 @@ class _APKInfoPageState extends State<APKInfoPage> {
           IconButton(
               icon: const Icon(Icons.search),
               onPressed: () {
-                getApkInfo();
+                loadApkInfo();
               }),
           IconButton(
               icon: const Icon(Icons.settings),
               onPressed: () {
                 Navigator.pushNamed(context, 'setting');
               }),
+          // 最右的空间
           const SizedBox(width: 50),
         ],
       ),
@@ -131,9 +181,66 @@ class _APKInfoPageState extends State<APKInfoPage> {
                 Card(
                     child: TitleValueRow(
                         title: "大小", value: "${fileSize ?? 0} bytes")),
-                Card(child: TitleValueRow(title: "包名", value: "")),
-                Card(child: TitleValueRow(title: "版本", value: "")),
-                const Card(child: ListTile(title: Text("Permissions"))),
+                Card(
+                    child: TitleValueRow(
+                        title: "包名", value: apkInfo?.packageName ?? "")),
+                Card(
+                    child: TitleValueRow(
+                        title: "应用名称", value: apkInfo?.label ?? "")),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Card(
+                      child: TitleValueRow(
+                          title: "最低SDK",
+                          value: "${apkInfo?.sdkVersion ?? ""}",
+                          titleFlex: 6),
+                    )),
+                    Expanded(
+                        child: Card(
+                            child: TitleValueRow(
+                                title: "目标SDK",
+                                value:
+                                    apkInfo?.targetSdkVersion?.toString() ?? "",
+                                titleFlex: 6))),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                        child: Card(
+                            child: TitleValueRow(
+                                title: "版本号",
+                                value: "${apkInfo?.versionCode ?? ""}",
+                                titleFlex: 6))),
+                    Expanded(
+                        child: Card(
+                            child: TitleValueRow(
+                                title: "版本名称",
+                                value: apkInfo?.versionName ?? "",
+                                titleFlex: 6))),
+                  ],
+                ),
+                Card(
+                    child: TitleValueRow(
+                        title: "屏幕尺寸",
+                        value: apkInfo?.supportsScreens.join(" ") ?? "")),
+                Card(
+                    child: TitleValueRow(
+                        title: "屏幕密度",
+                        value: apkInfo?.densities.join(" ") ?? "")),
+                Card(
+                    child: TitleValueRow(
+                        title: "ABI",
+                        value: apkInfo?.nativeCodes.join(" ") ?? "")),
+                Card(
+                    child: TitleValueRow(
+                        title: "语言列表",
+                        value: apkInfo?.locales.join(" ") ?? "")),
+                Card(
+                    child: TitleValueRow(
+                        title: "权限列表",
+                        value: apkInfo?.usesPermissions.join("\n") ?? "")),
               ],
             ),
           ))
@@ -147,11 +254,16 @@ class TitleValueRow extends StatefulWidget {
   final String title;
   final String value;
   final Widget? end;
+  final int titleFlex;
+  final int textFlex;
 
-  TitleValueRow({
+  const TitleValueRow({
+    super.key,
     required this.title,
     required this.value,
     this.end,
+    this.titleFlex = 2,
+    this.textFlex = 6,
   });
 
   @override
@@ -168,9 +280,10 @@ class _TitleValueRowState extends State<TitleValueRow> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Expanded(child: ListTile(title: Text(widget.title))),
         Expanded(
-          flex: 4,
+            flex: widget.titleFlex, child: ListTile(title: Text(widget.title))),
+        Expanded(
+          flex: widget.textFlex,
           child: SelectableText(widget.value),
         ),
         if (widget.end != null) widget.end!,
@@ -196,7 +309,7 @@ class MyApp extends StatelessWidget {
         Locale('zh', 'CN'), // 中文简体
         //其他Locales
       ],
-      title: 'Flutter Demo',
+      title: appTitle,
       initialRoute: "/",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurpleAccent),
